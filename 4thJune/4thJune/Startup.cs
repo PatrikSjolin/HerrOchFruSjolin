19a3f8866bbddc94101e5f92a0171c5b012a4a61
PatrikSjolin <patrik.sjolin@gmail.com>
2016-03-03 16:46:50 +0100

    Initial commit

---
+++ 4thJune/4thJune/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4thJune.Startup))]
+namespace _4thJune
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
